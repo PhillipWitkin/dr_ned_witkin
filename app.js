cd7d9a534329c930275a5e8286ad8b1e1748e44b
Phillip Witkin <pwitkin273@gmail.com>
2015-10-01 18:04:57 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var fs = require("fs")
+app.set('port', (process.env.PORT || 3000));
+// console.log("listening on port" + process.env.PORT || 3000)
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function(req,res){
+  var html = fs.readFileSync("./index.html", "utf8")
+  res.send(html)
+})
+
+var serverLog = app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
